61c2b5a218e91dffc7d655a63e30ad8cd62052ea
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2018-01-18 19:02:27 +0100

    feat(views): Introduced basic view workflow

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const compression = require('compression');
 const error = require('debug')('error');
 const express = require('express');
+const hbs = require('hbs');
 const info = require('debug')('info');
 const morgan = require('morgan');
+const path = require('path');
 const session = require('express-session');
 
 const Provider = require('oidc-provider');
@@ -27,6 +29,8 @@ const PORT = argv.p || argv.port || process.env.PORT || 3000;
 
 const app = express();
 app.set('trust proxy', 1);
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 app.use(morgan('combined'));
 app.use(compression({ level: 6 }));
@@ -39,11 +43,26 @@ app.use(session({
   store,
   unset: 'destroy',
 }));
+app.use('/assets', express.static('public'));
+
+hbs.registerPartials(path.resolve(__dirname, 'views/partials'));
+hbs.localsAsTemplateData(app);
+app.locals.url = `${DEV ? 'http://' : 'https://'}${DOMAIN}${DEV ? (`:${PORT}`) : ''}`;
 
 const oidc = new Provider(`${DEV ? 'http://' : 'https://'}${DOMAIN}${DEV ? (`:${PORT}`) : ''}`);
 oidc.initialize(config)
   .then(() => {
+    app.get('/interaction/:grantId', async (req, res) => {
+      const details = await oidc.interactionDetails(req);
+      const interaction = details.interaction.reason === 'consent_prompt' || details.interaction.reason === 'client_not_authorized';
+      info(details);
+
+      const view = interaction ? 'interaction' : 'login';
+      return res.render(view, details);
+    });
+
     app.use('/', oidc.callback);
+
     app.listen(PORT, (err) => {
       if (err) {
         throw new Error(`An error occurred: ${err.message || err}`);
