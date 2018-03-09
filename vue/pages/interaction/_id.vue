0c240003af7097f411f9533e2f78cbd6058ce749
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2018-03-09 16:57:32 +0100

    feat(nuxt): Added interaction template

---
+++ vue/pages/interaction/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <div class="card__header">
+      <h1>Authorize <span vue-if="client">"{{ client }}"</span></h1>
+    </div>
+    <div class="card__body">
+      <p>The application asks for permission to retrieve your user information:</p>
+      <form :action="`/interaction/${uuid}/confirm`" method="post">
+        
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData({ params, req }) {
+    console.log(req.session[params.id]);
+    return {
+      details: req.session[params.id],
+      uuid: params.id,
+    };
+  },
+}
+</script>
